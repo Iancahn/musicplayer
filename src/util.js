eb4cbf7db788a7759fcdfcdfcf1d7ab00998032d
Ian Cahn <iancahn@live.com>
2022-05-27 21:35:23 +0200

    Major Update v0.4b
    + Added Song data,
    + Installed UUID
    + Styled Song Components
    + Updated State
    + Updated Current State to reflect current song

---
+++ src/util.js
@@ -0,0 +1,62 @@
+import { v4 as uuidv4 } from "uuid";
+
+function chillHop() {
+    return [
+        {
+            name: "Kokon",
+            artist: "Plusma, Guillaume Muschalle",
+            cover: "https://chillhop.com/wp-content/uploads/2022/05/d8344de22563af8eaec8f544c14711592eabef26-1024x1024.jpg",
+            id: uuidv4(),
+            active: false,
+            color: ["#A3835C", "#7BBAB7"],
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=28895"
+        },
+        {
+            name: "Valse",
+            artist: "Plusma, Guillaume Muschalle",
+            cover: "https://chillhop.com/wp-content/uploads/2022/05/d8344de22563af8eaec8f544c14711592eabef26-1024x1024.jpg",
+            id: uuidv4(),
+            active: false,
+            color: ["#A3835C", "#7BBAB7"],
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=28897"
+        },
+        {
+            name: "Pampaya",
+            artist: "Plusma, Guillaume Muschalle",
+            cover: "https://chillhop.com/wp-content/uploads/2022/05/d8344de22563af8eaec8f544c14711592eabef26-1024x1024.jpg",
+            id: uuidv4(),
+            active: false,
+            color: ["#A3835C", "#7BBAB7"],
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=28899"
+        },
+        {
+            name: "Parasol",
+            artist: "Plusma, Guillaume Muschalle",
+            cover: "https://chillhop.com/wp-content/uploads/2022/05/d8344de22563af8eaec8f544c14711592eabef26-1024x1024.jpg",
+            id: uuidv4(),
+            active: false,
+            color: ["#A3835C", "#7BBAB7"],
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=28901"
+        },
+        {
+            name: "Bronea",
+            artist: "Plusma, Guillaume Muschalle",
+            cover: "https://chillhop.com/wp-content/uploads/2022/05/d8344de22563af8eaec8f544c14711592eabef26-1024x1024.jpg",
+            id: uuidv4(),
+            active: false,
+            color: ["#A3835C", "#7BBAB7"],
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=28903"
+        },
+        {
+            name: "Eclaircie",
+            artist: "Plusma, Guillaume Muschalle",
+            cover: "https://chillhop.com/wp-content/uploads/2022/05/d8344de22563af8eaec8f544c14711592eabef26-1024x1024.jpg",
+            id: uuidv4(),
+            active: false,
+            color: ["#A3835C", "#7BBAB7"],
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=28905"
+        }
+    ];
+}
+
+export default chillHop;
