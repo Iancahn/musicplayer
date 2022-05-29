4620c95234332fb50734abe88c8efe9b28628d35
Ian Cahn <iancahn@live.com>
2022-05-29 13:56:39 +0200

    Update Song Duration
    our TimeUpdateHandler is now also using a JS time format function to display both currentTime and duration of Song on our screen.

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -18,7 +18,12 @@ function Player({ currentSong, isPlaying, setIsPlaying }) {
     const timeUpdateHandler = (e) => {
         const current = e.target.currentTime;
         const duration = e.target.duration;
-        console.log(current);
+        setSongInfo({ ...songInfo, currentTime: current, duration })
+    }
+    const getTime = (time) => {
+        return (
+            Math.floor(time / 60) + ":" + ("0" + Math.floor(time % 60)).slice(-2)
+        );
     }
     // State  
     const [songInfo, setSongInfo] = useState({
@@ -29,16 +34,16 @@ function Player({ currentSong, isPlaying, setIsPlaying }) {
     return (
         <div className="player">
             <div className="time-control">
-                <p>Start Time</p>
+                <p>{getTime(songInfo.currentTime)}</p>
                 <input type="range"></input>
-                <p>End Time</p>
+                <p>{getTime(songInfo.duration)}</p>
             </div>
             <div className="play-control">
                 <FontAwesomeIcon className="skip-back" size="2x" icon={faAngleLeft} />
                 <FontAwesomeIcon onClick={playSongHandler} className="play" size="2x" icon={faPlay} />
                 <FontAwesomeIcon className="skip-forward" size="2x" icon={faAngleRight} />
             </div>
-            <audio onTimeUpdate={timeUpdateHandler} ref={audioRef} src={currentSong.audio}></audio>
+            <audio onLoadedMetadata={timeUpdateHandler} onTimeUpdate={timeUpdateHandler} ref={audioRef} src={currentSong.audio}></audio>
         </div >
     )
 }
