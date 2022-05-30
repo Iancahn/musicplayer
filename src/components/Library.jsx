92d1a32f41374ad23dc1861ec9e470f9a6305ce1
Ian Cahn <iancahn@live.com>
2022-05-30 21:14:09 +0200

    Library Song Select
    We can now select songs out of our library to our current Song.

--- src/components/Library.jsx
+++ src/components/Library.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import LibrarySong from './LibrarySong';
 
-const Library = ({ songs }) => {
+const Library = ({ songs, setCurrentSong, id }) => {
     return (
         <div className="library">
             <h2>Library</h2>
             <div className='library-songs'>
                 {songs.map(song =>
-                    <LibrarySong song={song} />
+                    <LibrarySong song={song} setCurrentSong={setCurrentSong} songs={songs} id={song.id} key={song.id} />
                 )}
             </div>
         </div>
