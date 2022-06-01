5d19bef59e4bb2aaf724ed14bd5daa91dfd08026
Ian Cahn <iancahn@live.com>
2022-06-01 23:12:17 +0200

    Small bugfix
    When the next song loads, sometimes it shows NaN while fetching new song. Updated it to show 0:00 if it has not received songInfo.duration

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -63,7 +63,7 @@ function Player({ currentSong, isPlaying, setIsPlaying, audioRef, songInfo, setS
             <div className="time-control">
                 <p>{getTime(songInfo.currentTime)}</p>
                 <input min={0} max={songInfo.duration || 0} value={songInfo.currentTime} onChange={dragHandler} type="range"></input>
-                <p>{getTime(songInfo.duration)}</p>
+                <p>{songInfo.duration ? getTime(songInfo.duration) : '0:00'}</p>
             </div>
             <div className="play-control">
                 <FontAwesomeIcon onClick={() => skipTrackHandler('skip-back')} className="skip-back" size="2x" icon={faAngleLeft} />
