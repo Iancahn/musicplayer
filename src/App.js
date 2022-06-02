d9dfa751ad25b1afa64e2bb1720b7a420fbd4892
Ian Cahn <iancahn@live.com>
2022-06-02 21:27:39 +0200

    Updated Input Styling
    Added our own Input slider styles. These styles replaced the default styles.

--- src/App.js
+++ src/App.js
@@ -22,13 +22,16 @@ function App() {
   const [songInfo, setSongInfo] = useState({
     currentTime: 0,
     duration: 0,
+    animationPercentage: 0,
   });
   const [libraryStatus, setLibraryStatus] = useState(false);
   // Find Handlers below
   const timeUpdateHandler = (e) => {
     const current = e.target.currentTime;
     const duration = e.target.duration;
-    setSongInfo({ ...songInfo, currentTime: current, duration })
+    //Calculate Percentage
+    const roundedCurrent = Math.round(current);
+    setSongInfo({ ...songInfo, currentTime: current, duration });
   }
 
   return (
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -62,7 +62,10 @@ function Player({ currentSong, isPlaying, setIsPlaying, audioRef, songInfo, setS
         <div className="player">
             <div className="time-control">
                 <p>{getTime(songInfo.currentTime)}</p>
-                <input min={0} max={songInfo.duration || 0} value={songInfo.currentTime} onChange={dragHandler} type="range"></input>
+                <div className="track">
+                    <input min={0} max={songInfo.duration || 0} value={songInfo.currentTime} onChange={dragHandler} type="range"></input>
+                    <div className="animate-track"></div>
+                </div>
                 <p>{songInfo.duration ? getTime(songInfo.duration) : '0:00'}</p>
             </div>
             <div className="play-control">
